d384bb0236b47ec8ba90075e4a40bd76b2488282
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-04-01 23:53:29 +0300

    Add files via upload

---
+++ JavaCore 2005 Very strange affairs/src/com/company/Solution.java
@@ -0,0 +1,165 @@
+package com.company;
+
+/*
+
+2005 Very strange affairs
+Strange errors occur when reading / writing Human-type objects.
+Understand what the matter is and correct them.
+
+Requirements:
+1. The read / write logic implemented in the save / load methods should work correctly if the assets list is null.
+2. The read / write logic implemented in the save / load methods should work correctly if the name field and asset list are not null.
+3. The Solution.Human class should not support the Serializable interface.
+4. The Solution.Human class must be public.
+5. The Solution.Human class should not support the Externalizable interface.
+6. The equals method must return true for two equal objects of type Human and false for different.
+7. The hashCode method must always return the same value for the same type object
+
+
+*/
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Странные ошибки
+При чтении/записи объектов типа Human возникают странные ошибки.
+Разберись в чем дело и исправь их.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //исправь outputStream/inputStream в соответствии с путем к твоему реальному файлу
+        try {
+            File your_file_name = new File ("e:\\6.txt");//File.createTempFile("your_file_name", null);
+            OutputStream outputStream = new FileOutputStream(your_file_name);
+            InputStream inputStream = new FileInputStream(your_file_name);
+
+            Human ivanov = new Human("Ivanov", new Asset("home"), new Asset("car"));
+            ivanov.save(outputStream);
+            outputStream.flush();
+
+            Human somePerson = new Human();
+            somePerson.load(inputStream);
+            //check here that ivanov equals to somePerson - проверьте тут, что ivanov и somePerson равны
+            System.out.println(ivanov.equals(somePerson));
+
+            inputStream.close();
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with save/load method");
+        }
+    }
+
+    public static class Human {
+        public String name;
+        public List<Asset> assets = new ArrayList<>();
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Human human = (Human) o;
+
+            //if (name == null ? !name.equals(human.name) : human.name == null) return false;
+            //return assets != null ? assets.equals(human.assets) : human.assets == null;
+            if (name == null || !name.equals(human.name)) return false;
+            if (assets == null || !assets.equals(human.assets)) return false;
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (assets != null ? assets.hashCode() : 0);
+            return result;
+        }
+
+        public Human() {
+        }
+
+        public Human(String name, Asset... assets) {
+            this.name = name;
+            if (assets != null) {
+                this.assets.addAll(Arrays.asList(assets));
+            }
+        }
+
+        public void save(OutputStream outputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.println(this.name);
+            if (this.assets.size() > 0) {
+                for (Asset current : this.assets)
+                    printWriter.println(current.getName());
+            }
+            printWriter.close();
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            this.name = reader.readLine();
+            String assetName;
+            while ((assetName = reader.readLine()) != null)
+                this.assets.add(new Asset(assetName));
+            reader.close();
+        }
+    }
+
+
+
+
+public static class Asset {
+    public Asset(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Asset asset = (Asset) o;
+
+        if (Double.compare(asset.price, price) != 0) return false;
+        return name != null ? name.equals(asset.name) : asset.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}}
+
+
